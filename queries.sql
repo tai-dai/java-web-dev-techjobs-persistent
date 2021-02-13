0c73bbbd9c2d2a72e65f0c062eb0036509cd87b3
Tai Smith <imadeanomelet@gmail.com>
2021-02-13 12:15:15 -0600

    Ayo pizza here

--- queries.sql
+++ queries.sql
@@ -7,10 +7,16 @@
 
 ## Part 2: Test it with SQL
 
-READ employers WHERE location = "St. Louis City";
+SELECT name
+FROM employer WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
 
 DROP TABLE jobs;
 
 ## Part 4: Test it with SQL
+
+SELECT name, description
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skill_id
+ORDER BY name ASC;
